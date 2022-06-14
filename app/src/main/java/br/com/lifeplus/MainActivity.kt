a9cbefb20cf9fb22fe9f76fb8368fccc65f72525
Vinicius-Mance <vinimanceprof@gmail.com>
2022-06-13 21:50:32 -0300

    tela login adicionada

--- app/src/main/java/br/com/lifeplus/MainActivity.kt
+++ app/src/main/java/br/com/lifeplus/MainActivity.kt
@@ -1,11 +1,24 @@
 package br.com.lifeplus
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnEntrar = findViewById<Button>(R.id.btnEntrar)
+
+        btnEntrar.setOnClickListener() {
+            goToLogin()
+        }
+    }
+
+    fun goToLogin(){
+        val link = Intent(this, LoginActivity::class.java)
+        startActivity(link)
     }
 }
