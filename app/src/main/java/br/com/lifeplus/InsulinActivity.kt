3fa4bae7f80a8aa027d255bc8d8f2bfe629ab8b1
Reginaldo-Rezende <Reginaldors777@hotmail.com>
2022-06-14 22:20:04 -0300

    mudança spinner testar

---
+++ app/src/main/java/br/com/lifeplus/InsulinActivity.kt
@@ -0,0 +1,17 @@
+package br.com.lifeplus
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.Toast
+
+class InsulinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_Insulin)
+
+    }
+}
